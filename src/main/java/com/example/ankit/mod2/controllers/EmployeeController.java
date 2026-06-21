f52855163cfd9e6164a5f0f6216eed60fcca5a22
Ankit Shaw <110915555+heyimankit@users.noreply.github.com>
2026-06-21 11:34:31 +0530

    Chapter 2.2

--- src/main/java/com/example/ankit/mod2/controllers/EmployeeController.java
+++ src/main/java/com/example/ankit/mod2/controllers/EmployeeController.java
@@ -10,15 +10,23 @@ import java.time.LocalDate;
 public class EmployeeController {
 //    @GetMapping(path="/getSecretMessage")
 //    public String getSecretMessage(){
-//        return "Secret Message: hellllooo";
+//        return "Secret Message: hello";
 //    }
     @GetMapping(path= "/{employeeId}")
     public EmployeeDTO getEmployeeByID(@PathVariable Long employeeId){
-        return new EmployeeDTO(employeeId,"Ankit","ankit@gmail.com",27, LocalDate.of(2026,07,25), true);
+        return new EmployeeDTO(employeeId,"Ankit","ankit@gmail.com",21, LocalDate.of(2026,06,20), true);
     }
     @GetMapping
-    public String getAllEmployees(@RequestParam Integer age,@RequestParam String name){
+    public String getAllEmployees(@RequestParam(required=false) Integer age,@RequestParam(required=false) String name){
         return "HI, my name is " +name+ " and age is "+age;
     }
-
+    @PostMapping
+    public EmployeeDTO addNewEmployee(@RequestBody EmployeeDTO inputEmployee){
+        inputEmployee.setId(100L);
+        return inputEmployee;
+    }
+    @PutMapping
+    public String updateEmployeeById(){
+        return "Hello from PUT";
+    }
 }
