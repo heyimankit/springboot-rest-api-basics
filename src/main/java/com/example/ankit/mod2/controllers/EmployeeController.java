157822af7acc0ae10b5b07f25917c1522b15473a
Ankit Shaw <110915555+heyimankit@users.noreply.github.com>
2026-06-23 21:43:31 +0530

    Add Employee REST API with Spring Data JPA

--- src/main/java/com/example/ankit/mod2/controllers/EmployeeController.java
+++ src/main/java/com/example/ankit/mod2/controllers/EmployeeController.java
@@ -1,29 +1,40 @@
 package com.example.ankit.mod2.controllers;
 
-import com.example.ankit.mod2.dto.EmployeeDTO;
+import com.example.ankit.mod2.entities.EmployeeEntity;
+import com.example.ankit.mod2.repositories.EmployeeRepository;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
+import java.util.List;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @RestController
 @RequestMapping(path="/employees")
 public class EmployeeController {
-      @GetMapping(path="/getSecretMessage")
+
+    @GetMapping(path="/getSecretMessage")
       public String getSecretMessage(){
           return "Secret Message: hello";
      }
-    @GetMapping(path= "/{employeeId}")
-    public EmployeeDTO getEmployeeByID(@PathVariable Long employeeId){
-        return new EmployeeDTO(employeeId,"Ankit","ankit@gmail.com",21, LocalDate.of(2026,06,20), true);
+     private final EmployeeRepository employeeRepository;
+
+     public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
+
+    @GetMapping("/id/{employeeId}")
+    public EmployeeEntity getEmployeeByID(@PathVariable Long employeeId) {
+        return employeeRepository.findById(employeeId).orElse(null);
+    }
+
     @GetMapping
-    public String getAllEmployees(@RequestParam(required=false) Integer age,@RequestParam(required=false) String name){
-        return "HI, my name is " +name+ " and age is "+age;
+    public List<EmployeeEntity> getAllEmployees(@RequestParam(required=false) Integer age, @RequestParam(required=false) String name){
+        return employeeRepository.findAll();
     }
+
     @PostMapping
-    public EmployeeDTO addNewEmployee(@RequestBody EmployeeDTO inputEmployee){
-        inputEmployee.setId(100L);
-        return inputEmployee;
+    public EmployeeEntity addNewEmployee(@RequestBody EmployeeEntity inputEmployee){
+        return employeeRepository.save(inputEmployee);
     }
     @PutMapping
     public String updateEmployeeById(){
